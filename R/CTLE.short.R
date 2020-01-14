3daa857909ce9844f25fdfc75d5b0e4bac6813f6
changwn <changwn.nku@gmail.com>
2020-01-14 15:11:49 -0500

    Update CTLE.short.R

--- R/CTLE.short.R
+++ R/CTLE.short.R
@@ -160,7 +160,7 @@ setMethod("CTLE",
 		print('----4----')
 		print(class(summary(refit(opt.fres), model=1,which = c("model","concomitant"))))
 		print('-------!!!!')
-		pvals_final=sapply(summary(refit(opt.fres))@components[[1]], function(x)(x[-1,4]))
+		pvals_final=sapply(refit(opt.fres)@components[[1]], function(x)(x[-1,4]))
                 result = new("RobMixReg", inds_in=inds_in,indout=outliers,ctleclusters=cates,compcoef=coffs_final,comppvals=pvals_final)
                 result@call <- mycall
                 result
