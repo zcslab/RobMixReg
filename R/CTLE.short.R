b057b62b9f9e482099671916ef94ea1fe22552b7
changwn <changwn.nku@gmail.com>
2020-01-14 15:18:41 -0500

    debug done

--- R/CTLE.short.R
+++ R/CTLE.short.R
@@ -136,10 +136,6 @@ setMethod("CTLE",
 		list1=sapply(ooo_list, function(x)list(1*((1:nobs)%in%x)))
 		opt.ind=which.min(sapply(list1, function(x)sum((x-Reduce("+",list1)/length(list1))^2)))
 		opt.fres=res_list[[opt.ind]]
-	print('01')
-	print(class(res_list))
-	print('02')
-	print(class(opt.fres))
 
 		outliers=ooo_list[[opt.ind]]
 		inds_in=setdiff(1:nobs,outliers)
@@ -147,92 +143,9 @@ setMethod("CTLE",
                 cates=vector("numeric",nobs)
                 cates[inds_in]=opt.fres@cluster
                 cates[outliers]=-1
-		print('here, pval!!!')
-		print(class(refit(opt.fres)))
-		print('---0---')
-		print(summary(refit(opt.fres)))
-		print('----1----')
-		print(typeof(refit(opt.fres)))
-		print('----2----')
-		print(class(summary(refit(opt.fres))))
-		print('----3----')
-		print(typeof(summary(refit(opt.fres))))
-		print('----4----')
-		print(class(summary(refit(opt.fres), model=1,which = c("model","concomitant"))))
-		print('-------!!!!')
 		pvals_final=sapply(refit(opt.fres)@components[[1]], function(x)(x[-1,4]))
                 result = new("RobMixReg", inds_in=inds_in,indout=outliers,ctleclusters=cates,compcoef=coffs_final,comppvals=pvals_final)
                 result@call <- mycall
                 result
 	}
-	# function(formula,data, nit=20,nc=2)
-	# {
-	# 	mycall = match.call();
-	# 	res_list=vector("list",nit)
-	# 	ooo_list=vector("list",nit)
-	# 	nx=ncol(data)-1; nobs = nrow(data)            # number of observations
-	# 	for(jj in 1:nit){
-	# 		flag=0; outliers=c(); ccc=0
-	# 		www=NULL
-	# 		for(ii in 1:nc){
-	# 			inds_random=sample(1:nobs,((nx+1)*2*5))
-	# 			mod_tmp=lmRob(formula=formula, data=data[inds_random,],control = lmRob.control(weight=c("Bisquare", "Bisquare")))
-	# 			www=cbind(www,dnorm(predict(mod_tmp, newdata=data),mean=0,sd=summary(mod_tmp)$sigma))
-	# 		}
-	# 		nc_tmp=nc
-	# 		while(flag==0 ){
-	# 			inds_in=1:nobs; outliers.old=outliers
-	# 			ccc=ccc+1
-	# 			outlier_list1=outlier_list=vector("list", nc_tmp)
-	# 			for(j in 1:nc_tmp){
-	# 				inds_in_tmp=which(apply(www, 1, which.max)==j)
-	# 				#ltsres = lm(formula=formula, data=data[inds_in_tmp,])
-	# 				ltsres = try(lmRob(formula=formula, data=data[inds_in_tmp,],control = lmRob.control(weight=c("Bisquare", "Bisquare"))),silent=TRUE)
-	# 				if(!inherits(ltsres, "try-error")){
-	# 	                                res12 = abs(cbind(1,as.matrix(data)) %*% matrix(c(ltsres$coefficients,-1),ncol=1))[,1] ##could we change it to nx!!!.
-	# 					oo1=inds_in_tmp[DeOut(res12[inds_in_tmp],"hampel")]
-	# 					oo2=inds_in_tmp[which(ltsres$T.M.weights==0)]
-	# 					outlier_list[[j]] = intersect(oo1,oo2)
-	# 				}
-	# 			}
-	# 			outliers=Reduce(c,outlier_list)
-	# 			if(length(outliers)>0){inds_in=c(1:nobs)[-outliers]}
-	# 			fres = flexmix_2(formula,data1=data[inds_in,],k=nc,mprior=0.1)            #
-	# 			nc_tmp=fres@k
-	# 			www = posterior(fres, newdata=data, unscale=TRUE)
-	# 			if(ccc>10  ){flag=1}
-	# 			if(length(outliers)==length(outliers.old)& ccc>1){
-	# 				if(length(outliers)==sum(outliers==outliers.old)){
-	# 					flag=1;
-	# 				}
-	# 			}
-	# 		}
-	# 		#print(sort(unique(outliers)))
-	# 		if(fres@k == nc){
-	# 			res_list[[jj]]=fres
-	# 			ooo_list[[jj]]=sort(unique(outliers))
-	# 		}
-	# 		#print(paste("The number of iteraction is", ccc))
-	# 	}
-	# 	ooo_list=ooo_list[][which(sapply(res_list, length)>0)]
-	# 	res_list=res_list[][which(sapply(res_list, length)>0)]
-	# 	llik=sapply(res_list, function(x)x@logLik)
-	# 	res_list=res_list[][order(llik,decreasing=TRUE)]
-	# 	ooo_list=ooo_list[][order(llik,decreasing=TRUE)]
-		
-	# 	list1=sapply(ooo_list, function(x)list(1*((1:nobs)%in%x)))
-	# 	opt.ind=which.min(sapply(list1, function(x)sum((x-Reduce("+",list1)/length(list1))^2)))
-	# 	opt.fres=res_list[[opt.ind]]
-
-	# 	outliers=ooo_list[[opt.ind]]
-	# 	inds_in=setdiff(1:nobs,outliers)
- #                coffs_final=rbind(parameters(opt.fres),apply(opt.fres@posterior$scaled,2,sum)/length(inds_in))
- #                cates=vector("numeric",nobs)
- #                cates[inds_in]=opt.fres@cluster
- #                cates[outliers]=-1
-	# 	pvals_final=sapply(summary(refit(opt.fres))@components[[1]], function(x)(x[-1,4]))
- #                result = new("RobMixReg", inds_in=inds_in,indout=outliers,ctleclusters=cates,compcoef=coffs_final,comppvals=pvals_final)
- #                result@call <- mycall
- #                result
-	# }
 )
