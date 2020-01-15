5673f36a84842f44e5af59b05399bac4cdcd0ae4
changwn <changwn.nku@gmail.com>
2020-01-14 23:42:34 -0500

    debug omit value for lr.method

--- R/outer.R
+++ R/outer.R
@@ -1,8 +1,9 @@
 
-rmr <- function(lr.method='CTLE', formula=NULL, data=NULL, nc=3, nit=20, tRatio=0.2, MaxIt=200, numini=20)
+rmr <- function(lr.method=NULL, formula=NULL, data=NULL, nc=3, nit=20, tRatio=0.2, MaxIt=200, numini=20)
 {
   if(is.null(formula)) stop('Please input formula!')
   if(is.null(data)) stop('data is null! Please input data')
+  if(is.null(lr.method)) stop('Please input the regression method from ("flexmix","TLE","CTLE","biSquare"). ')
 
   method.lib <- c('flexmix', 'TLE', 'CTLE', 'biSquare')
   if(!lr.method %in% method.lib) stop('lr.method must be chosen from "flexmix","TLE","CTLE","biSquare"!  ')
